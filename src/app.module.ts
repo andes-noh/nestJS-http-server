a7b20a391f8f602de1b53b9b26a81dd1be110513
andes-noh <nkj9693@gmail.com>
2022-08-28 21:46:34 +0900

    chore(app.module): module 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { TestModule } from './sample1/test.module'
+import { Test2Module } from './sample2/test2.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TestModule, ConfigModule.forRoot()],
+  imports: [TestModule, Test2Module, ConfigModule.forRoot()],
   providers: [],
   controllers: [],
 })
