2797512569e00d1bc84fc23682a55cbbcace0d99
andes-noh <nkj9693@gmail.com>
2022-09-07 13:46:09 +0900

    chore(index): default port 3000 -> 9000

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('swagger', app, document)
 
-  await app.listen(process.env.HTTP_PORT ?? 3000, '0.0.0.0')
+  await app.listen(process.env.HTTP_PORT ?? 9000, '0.0.0.0')
 }
 
 bootstrap()
