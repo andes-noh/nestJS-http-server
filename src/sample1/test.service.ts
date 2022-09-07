69541653c96bf751d5f39eec787c1437a7476704
andes-noh <nkj9693@gmail.com>
2022-09-07 15:45:54 +0900

    chore(service): db 연결 및 all data return

--- src/sample1/test.service.ts
+++ src/sample1/test.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 import { POSTDTO } from './test.dto'
+import { TestTable } from './test.entity'
 
 @Injectable()
 export class TestService {
-  constructor() {
-    //
+  constructor(
+    @InjectRepository(TestTable)
+    private readonly repository: Repository<TestTable>
+  ) {}
+
+  async findAll() {
+    return await this.repository.find()
   }
 
   async postTest(params: POSTDTO) {
--- src/sample2/test2.service.ts
+++ src/sample2/test2.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Test2Table } from './test2.entity'
 
 @Injectable()
 export class Test2Service {
-  constructor() {
+  constructor(
+    @InjectRepository(Test2Table)
+    private readonly repository: Repository<Test2Table>
+  ) {
     //
+  }
+
+  async findAll() {
+    return await this.repository.find()
   }
 
   async getTest() {
