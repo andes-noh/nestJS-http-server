adaf3e37570075bc8fbf657995f6f889914c11bf
andes-noh <nkj9693@gmail.com>
2022-09-07 13:20:48 +0900

    chore(app.module): db 관련 typeorm 내용 추가 및 env 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { TestModule } from './sample1/test.module'
 import { Test2Module } from './sample2/test2.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
 @Module({
-  imports: [TestModule, Test2Module, ConfigModule.forRoot()],
+  imports: [
+    TestModule,
+    Test2Module,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: String(process.env.DB_HOST),
+      port: Number(process.env.DB_PORT),
+      username: String(process.env.DB_USERNAME),
+      password: String(process.env.DB_PASSWORD),
+      database: String(process.env.DB_DATABASE),
+      synchronize: true,
+      autoLoadEntities: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   providers: [],
   controllers: [],
 })
